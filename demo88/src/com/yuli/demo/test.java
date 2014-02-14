da9ef2f39bf998e2b93b71d0e100ff8aebe1de13
ylxfc <yulixfc@gmail.com>
2014-02-15 00:49:52 +0800

    第一个版本

---
+++ demo88/src/com/yuli/demo/test.java
@@ -0,0 +1,10 @@
+package com.yuli.demo;
+
+/**
+ * Created by yuli on 14-2-15.
+ */
+public class test {
+    public static void main(String args[]) {
+        System.out.println("hehe!");
+    }
+}
